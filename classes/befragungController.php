26298448cf53532f66dc39ebd2e02b3357488898
armfa <51077660+armfa@users.noreply.github.com>
2020-05-06 15:47:31 +0200

    Funktion wie gefordert in aufgabenstellung ausgelagert

--- classes/befragungController.php
+++ classes/befragungController.php
@@ -1,14 +1,16 @@
 <?php
 //Fabrice Armbruster
 
+//______________________KLASSENBESCHREIBUNG______________________
 //Diese Klasse führt alle grundlegenden Funktionen in "indexBefragung.php" aus.
 //Hierzu zählen folgende Teile der Aufgabenstellung:
 
 // - Prozedur zur Belegung des Kommentarfelds für einen Studenten in einem Fragebogen.
 // - Prozedur, die für eine Matrikelnummer und einen Fragebogen die Einträge als abgeschlossen markiert.
-// -  Prozedur, um eine für eine Matrikelnummer die gemachte Punktevergabe einträgt (student, fragebogen, frage, punkte).
+// - Prozedur, um eine für eine Matrikelnummer die gemachte Punktevergabe einträgt (student, fragebogen, frage, punkte).
+// - Funktion, die prüft, ob ein Student einen Fragebogen ausfüllen darf. 
 
-// Hierfür werden die Datenbank Funktionen der Klasse Befragung genutzt. 
+// Hierfür werden die Datenbankfunktionen der Klasse "Befragung" genutzt. 
 
 class BefragungController extends Befragung{
 
@@ -46,4 +48,43 @@ class BefragungController extends Befragung{
         }
     }
 
+    //darf ein Student einen Fragebogen ausfüllen?
+    public function darfStudentFragebogenausfuellen($Matrikelnummer){
+                    //Dropdownauswahl des Fragebogens, welcher für diesen Student freigeschalten ist.     
+                    //Alle Fragebögen des Studenten, die abgegeben sind. 
+                    $fragebogen = $this->getFragebogenfromStudentAbgabestatusStnmt($_SESSION['matrikelnummer'], 1);
+                    //Alle Fragebögen die für den Studenten freigeschaltet sind, unabhängig der  bearbeitung oder abgabe
+                    $fragebogen2 = $this->getFragebogenfromStudent($_SESSION['matrikelnummer']);
+                    //Arrays vergleichen (MINUS) -> Unterschiede werden angezeigt
+                    //Sind sie gleich groß? -> Dann sind alle Bedfragungen schon abgegeben!
+                    if(count($fragebogen) != count($fragebogen2)){
+                        foreach ($fragebogen2 as $singleArray) {
+                        //Nach selben DS im anderen Array suchen
+        /*                 echo "Array-Ebene 1 ";
+                        print_r($singleArray);
+                        echo "</br>"; */
+                        if(count($fragebogen) != 0){
+                               foreach($fragebogen as $compareArray){
+        /*                     echo "Array-Ebene 2 ";
+                            print_r($compareArray);
+                            echo "</br>"; */
+                            //prüfen ob es keine treffer gibt
+                            if(count(array_diff_assoc($singleArray, $compareArray)) != 0){
+                                //Wenn es kein Treffer hat -> noch nicht abgegeben in array!
+                                $resultx[] = $singleArray;
+                                return $resultx;
+                            } 
+                        }
+                        }
+                        else{
+                            $resultx[] = $singleArray;
+                            return $resultx;
+                        }
+                    }
+                    }
+                    else{
+                        //Keine Treffer -> keine Fragebögen freigechaötet bzw alle schon abgeschlossen. 
+                        return $resultx = null;
+                    } 
+    }
 }
