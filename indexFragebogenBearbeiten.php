25e4922c9d6bbbb8a441698522079c60b6447d8e
Isi321 <57615942+Isi321@users.noreply.github.com>
2020-05-27 17:31:08 +0200

    Update indexFragebogenBearbeiten.php

--- indexFragebogenBearbeiten.php
+++ indexFragebogenBearbeiten.php
@@ -43,73 +43,48 @@ $kuerzel = $_GET['kuerzel'];
             <li><a href="indexLogin.php">Logout</a></li>
         </ul>
     </header>
-    
-    <h4>Fragebogen bearbeiten</h4>
-    <br>
-    
-    <h4>Frage löschen</h4>
-    <form action="" method="POST">
-        <!--Drop-Down Menü das alle Fragen anzeigt, die zu dem zuvor - auf der "indesFragebogen.php" Seite - ausgewählten Fragebogen gehören.-->
-        <select name='fragen'>
-
-        <?php
-        $fragenArray = $fragebogenObj->getFragenVonFragebogen($kuerzel);
-            
-        $i = 0;
-        while($i < count($fragenArray)){
-            echo "<option value='".$fragenArray[$i]['Fragenummer']."'>".$fragenArray[$i]['InhaltFrage']."</option>";
-            $i++;
-        } 
-        ?>
-        </select>
-
-        <input type="submit" name="frageLoeschen" value="Frage löschen" />
-    </form>
-    <br>
-
-    <h4>Frage hinzufügen</h4>
+   
+    <h3>Frage hinzufügen</h3>
     <form action="" method="POST">
         <input type="text" name="inhaltFrage" maxlength="100" required>
         <input type="submit" name="frageHinzufuegen" value="Frage hinzufügen" /><br>
     </form>
+    <br>
 
+    <h3>Frage löschen</h3>
+    <form action="" method="POST">
+        <!--Drop-Down Menü das alle Fragen anzeigt, die zu dem zuvor - auf der "indesFragebogen.php" Seite - ausgewählten Fragebogen gehören.-->
+        <select name='fragenr'>
+            <?php
+            $fragenArray = $fragebogenObj->getFragenVonFragebogen($kuerzel);
+            $i = 0;
+            while($i < count($fragenArray)){
+                echo "<option value='" . $fragenArray[$i]['Fragenummer'] . "'>" . $fragenArray[$i]['InhaltFrage'] . "</option>";
+                $i++;
+            } 
+            ?>
+        </select>
+
+        <input type="submit" name="frageLoeschen" value="Frage löschen" />
+    </form>
 </body>
-</html>
 
 <?php
-
-// Frage löschen
-if (isset($_POST['frageLoeschen'])) {
-    // Die im Drop-Down Menü ausgewählte Frage wird gelöscht.
-    $fragenummer = $_POST["fragen"];
-    $fragebogenObj->deleteFrage($fragenummer);
-    // Hier ist kein Fehlerfall möglich, deshalb ist der Löschvorgang immer erfolgreich.
-    header("Location: ../DB2__NK_PHP/indexFragebogenBearbeiten.php?u=success&kuerzel=$kuerzel");
-    exit();
-}
-  
-// Fehlermeldung zu Frage löschen
-if (!isset($_GET['u'])) {
-    // Falls nicht, wird nichts gemacht und das Skript abgebrochen. 
-} else { 
-    // Falls ein GET existiert, wird nach der Zuordnung ausgewertet.
-    $frageLoeschen = $_GET['u'];
-    // Die Meldung wird an der Oberfläche ausgegeben.
-    if ($frageLoeschen  == "success") {
-        echo "<p class='success'>Sie haben die Frage erfolgreich gelöscht!</p>";
-        exit();
-    }
-} 
-
 // Frage hinzufügen
 if (isset($_POST['frageHinzufuegen'])) {
     // Wenn der Button geklickt wurde, wird das Textfeld "inhaltFrage" einer Variable zugeordnet.
     $inhaltFrage = $_POST["inhaltFrage"];
+    // Prüfen, ob das Textfeld leer ist.
+    if (empty($inhaltFrage)) {
+        header("Location: ../DB2__NK_PHP/indexFragebogenBearbeiten.php?login=empty");
+        exit();
+    }
     // Prüfen, ob die Frage im Fragebogen schon existiert.
-    if ($fragebogenObj->checkObFrageExistiert($inhaltFrage, $kuerzel)) {
+    elseif ($fragebogenObj->checkObFrageExistiert($inhaltFrage, $kuerzel)) {
         header("Location: ../DB2__NK_PHP/indexFragebogenBearbeiten.php?s=nosuccess&kuerzel=$kuerzel");
         exit();
-    } else {
+    } 
+    else {
         // Die Frage wird dem Fragebogen hinzugefügt.
         $fragebogenObj->setFrage($inhaltFrage, $kuerzel);
         header("Location: ../DB2__NK_PHP/indexFragebogenBearbeiten.php?s=success&kuerzel=$kuerzel");
@@ -124,12 +99,42 @@ if (!isset($_GET['s'])) {
     // Falls ein GET existiert, wird nach der Zuordnung ausgewertet.
     $frageHinzufuegen = $_GET['s'];
     // Je nachdem, was für ein Fehler aufgetreten ist oder ob der Vorgang erfolgreich war, wird eine Meldung an der Oberfläche ausgegeben.
-    if ($frageHinzufuegen == "nosuccess") {
+    if ($frageHinzufuegen == "empty") {
+        echo "<p class='error'>Bitte füllen Sie das Textfeld aus!</p>";
+    } elseif ($frageHinzufuegen == "nosuccess") {
         echo "<p class='error'>Diese Frage existiert bereits!</p>";
-        exit();
     } elseif ($frageHinzufuegen == "success") {
         echo "<p class='success'>Sie haben die Frage/n erfolgreich angelegt!</p>";
+    }
+} 
+
+// Frage löschen
+if (isset($_POST['frageLoeschen'])) {
+    // Die im Drop-Down Menü ausgewählte Frage wird gelöscht.
+    $fragenummer = $_POST["fragenr"];
+    // Prüfen, ob der Frage bereits in Bearbeitung ist.
+    if($fragebogenObj->checkObFrageInBefragung($fragenummer) != false){
+        header("Location: ../DB2__NK_PHP/indexFragebogenBearbeiten.php?u=nosuccess&kuerzel=$kuerzel");
         exit();
+    } else{
+        $fragebogenObj->deleteFrage($fragenummer);
+        header("Location: ../DB2__NK_PHP/indexFragebogenBearbeiten.php?u=success&kuerzel=$kuerzel");
+        exit();
+    }
+}
+  
+// Fehlermeldung zu Frage löschen
+if (!isset($_GET['u'])) {
+    // Falls nicht, wird nichts gemacht und das Skript abgebrochen. 
+} else { 
+    // Falls ein GET existiert, wird nach der Zuordnung ausgewertet.
+    $frageLoeschen = $_GET['u'];
+    // Je nachdem, was für ein Fehler aufgetreten ist oder ob der Vorgang erfolgreich war, wird eine Meldung an der Oberfläche ausgegeben.
+    if ($frageLoeschen  == "nosuccess") {
+        echo "<p class='success'>Sie können die Frage nicht löschen, da diese bereits in der Bearbeitung durch die Studenten ist!</p>";
+    } elseif ($frageLoeschen  == "success") {
+        echo "<p class='success'>Sie haben die Frage erfolgreich gelöscht!</p>";
     }
 } 
 ?>
+</html>
