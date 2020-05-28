adf0f59a47140b0a7a6036038e7540794bb451b1
Isi321 <57615942+Isi321@users.noreply.github.com>
2020-05-28 11:25:05 +0200

    Update indexFragenHinzufuegen.php

--- indexFragenHinzufuegen.php
+++ indexFragenHinzufuegen.php
@@ -4,14 +4,14 @@
 
 //______________________KLASSENBESCHREIBUNG______________________
 // Diese PHP-Seite ist für das Hinzufügen der Fragen zum Fragebogen zuständig.
-// Es werden dafür Funktionen in den Klasse "Fragebogen.class" ausgelagert. 
+// Es werden dafür Funktionen in die Klasse "Fragebogen.class.php" ausgelagert. 
 
 include_once 'classes/dbh.class.php';
 
-// Diese Seite akzeptiert nur Benutzer
+// Diese Seite akzeptiert nur Benutzer.
 if (!isset($_SESSION['benutzername'])) {
-    //Falls Benutzer nicht eingeloggt wird dieser auf die index-Seite weitergeleitet.
-    //Ist dieser dort auch nicht eingeloggt auf die Login-Seite. 
+    // Falls Benutzer nicht eingeloggt sind, wird dieser auf die index-Seite weitergeleitet.
+    // Ist dieser dort auch nicht eingeloggt, wird dieser auf die Login-Seite weitergeleitet.
     header("Location: ../DB2__NK_PHP/index.php");
     exit();
 }
@@ -36,7 +36,7 @@ $kuerzel = $_GET['kuerzel'];
 </head>
 
 <body>
-    <!--Link um zurück auf die Startseite zu kommen bzw. Logout-->
+    <!--Link um zurück auf die Startseite bzw. Logout zu kommen-->
     <header style="background-color:lightGray;">
         <ul>
             <li><a href="indexFragebogen.php">Zurück zum Fragebogen</a></li>
@@ -73,12 +73,12 @@ if (isset($_POST['fragenHinzufuegen'])) {
             $inhaltFrage = $inhaltFragenArray[$z];
             // Prüfen, ob das Textfeld leer ist.
             if (empty($inhaltFrage)) {
-                header("Location: ../DB2__NK_PHP/indexFragenHinzufuegen.php?s=empty&kuerzel=$kuerzel&anzahlFragen=$anzahlFragen");
+                header("Location: ../DB2__NK_PHP/indexFragenHinzufuegen.php?fragenHinzufuegen=empty&kuerzel=$kuerzel&anzahlFragen=$anzahlFragen");
                 exit();
             }
             // Prüfen, ob die Frage im Fragebogen schon existiert.
             elseif ($fragebogenObj->checkObFrageExistiert($inhaltFrage, $kuerzel)) {
-                header("Location: ../DB2__NK_PHP/indexFragenHinzufuegen.php?s=nosuccess&kuerzel=$kuerzel&anzahlFragen=$anzahlFragen");
+                header("Location: ../DB2__NK_PHP/indexFragenHinzufuegen.php?fragenHinzufuegen=nosuccess&kuerzel=$kuerzel&anzahlFragen=$anzahlFragen");
                 exit();
             } 
             else {
@@ -87,21 +87,21 @@ if (isset($_POST['fragenHinzufuegen'])) {
             }
         }
         // Wenn die Fragen erfolgreich hinzugefügt wurden, wird eine Meldung ausgegeben.
-        header("Location: ../DB2__NK_PHP/indexFragenHinzufuegen.php?s=success&kuerzel=$kuerzel&anzahlFragen=$anzahlFragen");
+        header("Location: ../DB2__NK_PHP/indexFragenHinzufuegen.php?fragenHinzufuegen=success&kuerzel=$kuerzel&anzahlFragen=$anzahlFragen");
         exit();
     } else {
         // Wenn die Länge des Arrays nicht der Anzahl der Fragen entspricht, wird einer Fehlermeldung ausgegeben.
-        header("Location: ../DB2__NK_PHP/indexFragenHinzufuegen.php?s=wrong&kuerzel=$kuerzel&anzahlFragen=$anzahlFragen");
+        header("Location: ../DB2__NK_PHP/indexFragenHinzufuegen.php?fragenHinzufuegen=wrong&kuerzel=$kuerzel&anzahlFragen=$anzahlFragen");
         exit();
     }
 }
 
 // Fehlermeldung zu Fragen hinzufügen
-if (!isset($_GET['s'])) {
+if (!isset($_GET['fragenHinzufuegen'])) {
     // Falls nicht, wird nichts gemacht und das Skript abgebrochen. 
 } else {
     // Falls ein GET existiert, wird nach der Zuordnung ausgewertet.
-    $frageAnlegen = $_GET['s'];
+    $frageAnlegen = $_GET['fragenHinzufuegen'];
     // Je nachdem, was für ein Fehler aufgetreten ist oder ob der Vorgang erfolgreich war, wird eine Meldung an der Oberfläche ausgegeben.
     if ($frageAnlegen == "empty") {
         echo "<p class='error'>Bitte füllen Sie die Textfelder aus!</p>";
